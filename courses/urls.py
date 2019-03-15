5982252a06d02db146e21cdead35d789734612f6
kimeualexis <kimeualexis@gmail.com>
2019-03-15 19:00:48 +0300

    module formsets

--- courses/urls.py
+++ courses/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url('create/', views.CourseCreateView.as_view(), name='course_create'),
     url('(?P<pk>\d+)/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     url('(?P<pk>\d+)/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
+    url('(?P<pk>\d+)/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
 ]
